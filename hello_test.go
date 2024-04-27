5e2eb871d5f35ab9bc41416d9f874ee3f772d8c1
patdel0 <zefiru@protonmail.com>
2024-04-27 01:01:27 +0100

    Initial commit

---
+++ hello_test.go
@@ -0,0 +1,24 @@
+// package main
+
+// import "testing"
+
+// func TestHello(t *testing.T) {
+// 	got := Hello()
+// 	want := "Hello, world"
+
+// 	if got != want {
+// 		t.Errorf("got %q want %q", got, want)
+// 	}
+// }
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello()
+	want := "Hello, world"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
